5261afcedb4b0c89270d3ca51d8b6ae260be6f13
Steven Varela <stevenvarela98@gmail.com>
2023-02-09 20:52:25 -0400

    :wrench: paypal button

--- src/containers/payment/Index.jsx
+++ src/containers/payment/Index.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+import AppContext from '../../context/AppContext';
 
 const Payment = () => {
+  const { state } = useContext(AppContext);
+  const {buyer} = state;
   return (
     <div className='flex'>
       <div className="w-screen flex flex-col lg:flex-row bg-gray-200 p-10 lg:p-20">
@@ -9,6 +14,17 @@ const Payment = () => {
           <div className="Payment-button">
             Boton de pago con Paypal
           </div>
+          <PayPalScriptProvider options={{ "client-id": "test" }}>
+            <PayPalButtons
+              style={{ layout: "horizontal" }}
+              // onApprove={(data, actions) => {
+              //   return actions.order.capture().then((details) => {
+              //     const name = details.payer.name.given_name;
+              //     alert(`Transaction completed by ${name}`);
+              //   });
+              // }}
+            />
+          </PayPalScriptProvider>
         </section>
         <div />
       </div>
